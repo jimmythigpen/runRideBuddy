9275c70520f7ae51f2413022391799461837177d
Jimmy Thigpen <jmthigpen@me.com>
2015-03-11 12:29:05 -0400

    created index route and updated create page

---
+++ app/controllers/home/create.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  startMarker: [23, 24, 25, 26, 27],
+  finishMarker: [23, 24, 25, 26, 27],
+});
--- config/environment.js
+++ config/environment.js
@@ -25,7 +25,8 @@ module.exports = function(environment) {
       'connect-src': "'self' *",
       'img-src': "'self' *",
       'style-src': "'self' *",
-      'media-src': "'self'"
+      'media-src': "'self'",
+      'frame-src': "'self' *"
     }
   };
 
