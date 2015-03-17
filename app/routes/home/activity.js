f9473816ae1824f73da09d501b3aca6ee0114287
Jimmy Thigpen <jmthigpen@me.com>
2015-03-17 09:36:13 -0400

    working on RSVP hash on activiy route

--- app/routes/home/activity.js
+++ app/routes/home/activity.js
@@ -1,17 +1,17 @@
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 import Ember from 'ember';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  model: function(params) {
-    return this.store.find('activity', params.activity_id);
-    }
-});
-
 // export default Ember.Route.extend(AuthenticatedRouteMixin, {
 //   model: function(params) {
-//     return new Ember.RSVP.hash({
-//       activity: this.store.find('activity', params.activity_id),
-//       users: this.store.findAll('user'),
-//     });
-//   }
+//     return this.store.find('activity', params.activity_id);
+//     }
 // });
+
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  model: function(params) {
+    return new Ember.RSVP.hash({
+      activity: this.store.find('activity', params.activity_id),
+      users: this.store.findAll('user'),
+    });
+  }
+});
