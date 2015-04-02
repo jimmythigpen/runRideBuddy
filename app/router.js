94637c897527fbfc1f220c0387ad89131769656b
Jimmy Thigpen <jmthigpen@me.com>
2015-04-02 12:29:07 -0400

    updated home route

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('home', {path: '/'}, function() {
+  this.route('home', {path: '/runRideBuddy'}, function() {
     this.route('account');
     this.route('activity', {path: '/activity/:activity_id'});
     this.route('create');
