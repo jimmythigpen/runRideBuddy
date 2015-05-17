c2b61704810742e29af43caf2c1cf6cce079087f
Jimmy Thigpen <jmthigpen@me.com>
2015-05-17 14:17:01 -0400

    fixed login default photo

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -6,36 +6,52 @@ export default Ember.Controller.extend({
   actions: {
 
     register: function(){
-      this.uploadPicture().then(function(imageFile){
-        var data = this.getProperties('firstName', 'lastName', 'username', 'password');
-        data.email = data.username;
-        data.profilePic = {
-         "name": imageFile.name,
-         "__type": "File"
-       };
-        ajax({
-          url:  "https://api.parse.com/1/users",
-          type: "POST",
-          data: JSON.stringify(data),
-          contentType: 'application/json'
-        }).then(function(response){
-           this.session.authenticate('authenticator:parse-email', {
-           sessionToken: response.sessionToken
-          });
+      if (this.uploadPicture() !== undefined) {
+        this.uploadPicture().then(function(imageFile){
+          var data = this.getProperties('firstName', 'lastName', 'username', 'password');
+          data.email = data.username;
+          data.profilePic = {
+           "name": imageFile.name,
+           "__type": "File"
+         };
+          ajax({
+            url:  "https://api.parse.com/1/users",
+            type: "POST",
+            data: JSON.stringify(data),
+            contentType: 'application/json'
+          }).then(function(response){
+             this.session.authenticate('authenticator:parse-email', {
+             sessionToken: response.sessionToken
+            });
+           }.bind(this));
          }.bind(this));
-       }.bind(this));
+       } else {
+       var data = this.getProperties('firstName', 'lastName', 'username', 'password');
+         data.email = data.username;
+         ajax({
+           url:  "https://api.parse.com/1/users",
+           type: "POST",
+           data: JSON.stringify(data),
+           contentType: 'application/json'
+         }).then(function(response){
+            this.session.authenticate('authenticator:parse-email', {
+            sessionToken: response.sessionToken
+           });
+          }.bind(this));
+        }
       }
-
     },
 
       uploadPicture: function() {
-      var file = this.get('accountPictureFile');
-      return ajax({
-        url: "https://api.parse.com/1/files/" + file.name,
-        type: "POST",
-        contentType: file.type,
-        data: file,
-        processData: false
-      });
+      if (this.get('accountPictureFile')) {
+        var file = this.get('accountPictureFile');
+        return ajax({
+          url: "https://api.parse.com/1/files/" + file.name,
+          type: "POST",
+          contentType: file.type,
+          data: file,
+          processData: false
+        });
+      }
     }
 });
